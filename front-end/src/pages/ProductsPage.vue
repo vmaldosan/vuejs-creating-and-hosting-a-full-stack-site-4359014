fbd308be967f50a37925108dcd71c863a2629498
Victor Maldonado <vmaldosan@gmail.com>
2024-09-18 14:51:50 +0000

    Lesson 1.11

--- front-end/src/pages/ProductsPage.vue
+++ front-end/src/pages/ProductsPage.vue
@@ -6,7 +6,9 @@
 			<img :src="product.imageName" />
 			<h3 class="product-name">{{ product.name }}</h3>
 			<p class="product-price">{{ product.price }}</p>
-			<button>View details</button>
+			<router-link :to="'/products/' + product.id">
+				<button>View details</button>
+			</router-link>
 		</div>
 	</div>
 </template>
