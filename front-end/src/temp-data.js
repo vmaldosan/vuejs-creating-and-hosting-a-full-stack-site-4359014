f3c2ac5b064971abd4cf84704537d5caf158323c
Victor Maldonado <vmaldosan@gmail.com>
2024-09-18 11:36:42 +0000

    Lesson 1.9

--- front-end/src/temp-data.js
+++ front-end/src/temp-data.js
@@ -7,6 +7,21 @@ import delicateBottle from "./assets/delicate-bottle.webp";
 import mediumBottle from "./assets/medium-bottle.webp";
 import boldBottle from "./assets/bold-bottle.webp";
 
+export const cartItems = [
+	{
+		id: "234",
+		name: "Lemon",
+		price: "$26.00",
+		imageName: lemonBottle,
+	},
+	{
+		id: "345",
+		name: "Garlic",
+		price: "$26.00",
+		imageName: garlicBottle,
+	},
+];
+
 export const products = [
 	{
 		id: "123",
