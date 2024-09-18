1b8c6477e891ed3f2b197eb8a047ef9318d27c89
Victor Maldonado <vmaldosan@gmail.com>
2024-09-18 15:36:32 +0000

    Lesson 1.13

--- front-end/src/pages/ProductsPage.vue
+++ front-end/src/pages/ProductsPage.vue
@@ -1,23 +1,18 @@
 <template>
 	<h1>Products</h1>
 
-	<div class="grid-wrap">
-		<div class="product-item" v-for="product in products" :key="product.id">
-			<img :src="product.imageName" />
-			<h3 class="product-name">{{ product.name }}</h3>
-			<p class="product-price">{{ product.price }}</p>
-			<router-link :to="'/products/' + product.id">
-				<button>View details</button>
-			</router-link>
-		</div>
-	</div>
+	<ProductsList :products="products" />
 </template>
 
 <script>
 import { products } from "../temp-data";
+import ProductsList from "../components/ProductsList.vue";
 
 export default {
 	name: "ProductsPage",
+	components: {
+		ProductsList,
+	},
 	data() {
 		return {
 			products,
