19759a1c82fe08584e3cc7bd7f6adb2e40dc609c
Victor Maldonado <vmaldosan@gmail.com>
2024-09-18 11:46:04 +0000

    Lesson 1.10

---
+++ front-end/src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="nav-bar">
+		<router-link to="/products" class="products-link">
+			<div class="logo-wrap">
+				<img :src="logo" />
+			</div>
+		</router-link>
+	</div>
+
+	<router-link to="/cart" class="cart-link">
+		<button>Shopping cart</button>
+	</router-link>
+</template>
+
+<script>
+import logo from "@/assets/logo-hexagon.svg";
+
+export default {
+	name: "NavBar",
+	data() {
+		return {
+			logo,
+		};
+	},
+};
+</script>
