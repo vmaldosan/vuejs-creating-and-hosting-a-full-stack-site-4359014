3a0ee7c03e5035dd4e0f0cbb8b31ba60005b0173
Victor Maldonado <vmaldosan@gmail.com>
2024-09-19 10:53:57 +0000

    Lesson 2.7

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,15 @@
 import express from "express";
-import { cartItems, products } from "./temp-data.js";
+import {
+	cartItems as cartItemsRaw,
+	products as productsRaw,
+} from "./temp-data.js";
+
+let cartItems = cartItemsRaw;
+let products = productsRaw;
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/hello", (req, res) => {
 	res.send("Hey yourself!");
@@ -19,6 +27,19 @@ app.get("/products/:productId", (req, res) => {
 	const productId = req.params.productId;
 	const product = products.find((product) => product.id === productId);
 	res.json(product);
+});
+
+app.post("/cart", (req, res) => {
+	const productId = req.body.id;
+	const product = products.find((product) => product.id === productId);
+	cartItems.push(product);
+	res.json(cartItems);
+});
+
+app.delete("/cart/:productId", (req, res) => {
+	const productId = req.params.productId;
+	cartItems = cartItems.filter((product) => product.id !== productId);
+	res.json(cartItems);
 });
 
 app.listen(8000, () => {
